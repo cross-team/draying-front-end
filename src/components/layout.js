818919b8c9d2c4653245e49b3f4248d40c187481
Sebastian <sebastian@softdev.team>
2019-12-19 14:52:46 -0500

    added material ui app bar

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import NavBar from './nav-bar'
 import Container from '@material-ui/core/Container'
+import './layout.css'
 
 const Layout = ({ children }) => (
-  <Container maxWidth="lg">
+  <>
     <NavBar />
-    {children}
-  </Container>
+    <Container maxWidth="lg">{children}</Container>
+  </>
 )
 export default Layout
