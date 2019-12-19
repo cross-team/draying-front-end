4e96311fbc679db96b7a3d8b069c84382972a613
Sebastian <sebastian@softdev.team>
2019-12-19 14:59:35 -0500

    minor tweaks

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -1,53 +1,5 @@
 import { Link, navigate } from 'gatsby'
 import { isLoggedIn, logout } from '../services/auth'
-import Button from '@material-ui/core/Button'
-
-// export default () => {
-//   const content = { message: '', login: true }
-//   if (isLoggedIn()) {
-//     content.message = `Hello, you are logged in`
-//   } else {
-//     content.message = 'You are not logged in'
-//   }
-//   return (
-//     <div
-//       style={{
-//         display: 'flex',
-//         flex: '1',
-//         justifyContent: 'space-between',
-//         borderBottom: '1px solid #d1c1e0',
-//       }}
-//     >
-//       <span>{content.message}</span>
-//       <nav>
-//         <Link to="/">
-//           <Button>Home</Button>
-//         </Link>
-//         {` `}
-//         {isLoggedIn() ? (
-//           <>
-//             <Link to="/app/profile">
-//               <Button>Profile</Button>
-//             </Link>
-//             <Link to="/app/dispatch">
-//               <Button>Dispatch</Button>
-//             </Link>
-//             <Link
-//               to="/"
-//               onClick={event => {
-//                 event.preventDefault()
-//                 logout(() => navigate(`/app/login`))
-//               }}
-//             >
-//               <Button>Logout</Button>
-//             </Link>
-//           </>
-//         ) : null}
-//       </nav>
-//     </div>
-//   )
-// }
-
 import React from 'react'
 import { fade, makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
@@ -61,7 +13,6 @@ import Menu from '@material-ui/core/Menu'
 import MenuIcon from '@material-ui/icons/Menu'
 import SearchIcon from '@material-ui/icons/Search'
 import AccountCircle from '@material-ui/icons/AccountCircle'
-import MailIcon from '@material-ui/icons/Mail'
 import NotificationsIcon from '@material-ui/icons/Notifications'
 import MoreIcon from '@material-ui/icons/MoreVert'
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import { isLoggedIn, getUser } from '../services/auth'
-
+import Button from '@material-ui/core/Button'
 import Layout from '../components/layout'
 
 export default () => (
@@ -11,12 +11,17 @@ export default () => (
       {isLoggedIn() ? (
         <>
           You are logged in, so check your{' '}
-          <Link to="/app/profile">profile</Link>
+          <Link to="/app/profile">
+            <Button variant="contained">profile</Button>
+          </Link>
         </>
       ) : (
         <>
-          You should <Link to="/app/login">log in</Link> to see restricted
-          content
+          You should{' '}
+          <Link to="/app/login">
+            <Button variant="contained">Log in</Button>
+          </Link>{' '}
+          to see restricted content
         </>
       )}
     </p>
