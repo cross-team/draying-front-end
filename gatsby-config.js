edfaf79fe126bc5ec522247fe814754689b76c0c
Marcello Paiva <marcello.paiva@gmail.com>
2020-01-06 16:00:13 -0500

    added @cross.team/gatsby-theme-material-ui and @cross.team/gatsby-theme-eslint to the project

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,5 +38,7 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
+    `@cross.team/gatsby-theme-material-ui`,
+    `@cross.team/gatsby-theme-eslint`,
   ],
 }
