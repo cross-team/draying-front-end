f3cae61132138a4a756b500bdd24af11c532872f
Marcello Paiva <marcello.paiva@gmail.com>
2020-01-07 14:02:25 -0500

    started adding drawer menu and logout menu

---
+++ src/components/drawer-menu.js
@@ -0,0 +1,160 @@
+import React from 'react'
+import {
+  makeStyles,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  Collapse
+} from '@material-ui/core/'
+import LessIcon from '@material-ui/icons/ExpandLess';
+import MoreIcon from '@material-ui/icons/ExpandMore';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+  },
+  nested: {
+    paddingLeft: theme.spacing(4),
+  },
+}));
+
+export default function DrawerMenu(props) {
+  const classes = useStyles()
+  const [dispatching, setDispatching] = React.useState(false);
+  const [drivers, setDrivers] = React.useState(false);
+  const [orders, setOrders] = React.useState(false);
+  const [clients, setClients] = React.useState(false);
+  const [preferences, setPreferences] = React.useState(false);
+
+  const handleDispatching = () => {
+    setDispatching(!dispatching)
+  }
+
+  const handleDrivers = () => {
+    setDrivers(!drivers)
+  }
+
+  const handleOrders = () => {
+    setOrders(!orders)
+  }
+
+  const handleClients = () => {
+    setClients(!clients)
+  }
+
+  const handlePreferences = () => {
+    setPreferences(!preferences)
+  }
+
+  return (
+    <Drawer open={props.open} onClose={props.toggleDrawer(false)}>
+      <List>
+
+        <ListItem button onClick={handleDispatching}>
+          <ListItemText primary="Dispatching" />
+          {dispatching ? <LessIcon /> : <MoreIcon />}
+        </ListItem>
+        <Collapse in={dispatching} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <ListItem button className={classes.nested}>
+              <ListItemText primary="Drivers" />
+            </ListItem>
+            <ListItem button className={classes.nested}>
+              <ListItemText primary="Orders" />
+            </ListItem>
+          </List>
+        </Collapse>
+
+        <ListItem button onClick={handleDrivers}>
+          <ListItemText primary="Drivers" />
+          {drivers ? <LessIcon /> : <MoreIcon />}
+        </ListItem>
+        <Collapse in={drivers} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <ListItem button className={classes.nested}>
+              <ListItemText primary="Drivers" />
+            </ListItem>
+            <ListItem button className={classes.nested}>
+              <ListItemText primary="Trucks" />
+            </ListItem>
+            <ListItem button className={classes.nested}>
+              <ListItemText primary="Payments" />
+            </ListItem>
+            <ListItem button className={classes.nested}>
+              <ListItemText primary="Add Payment" />
+            </ListItem>
+          </List>
+        </Collapse>
+
+        <ListItem button onClick={handleOrders}>
+          <ListItemText primary="Orders" />
+          {orders ? <LessIcon /> : <MoreIcon />}
+        </ListItem>
+        <Collapse in={orders} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <ListItem button className={classes.nested}>
+              <ListItemText primary="Orders" />
+            </ListItem>
+            <ListItem button className={classes.nested}>
+              <ListItemText primary="Add Order" />
+            </ListItem>
+            <ListItem button className={classes.nested}>
+              <ListItemText primary="Order Status" />
+            </ListItem>
+          </List>
+        </Collapse>
+
+        <ListItem button onClick={handleClients}>
+          <ListItemText primary="Clients" />
+          {clients ? <LessIcon /> : <MoreIcon />}
+        </ListItem>
+        <Collapse in={clients} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <ListItem button className={classes.nested}>
+              <ListItemText primary="Clients" />
+            </ListItem>
+            <ListItem button className={classes.nested}>
+              <ListItemText primary="Orders" />
+            </ListItem>
+            <ListItem button className={classes.nested}>
+              <ListItemText primary="Invoices" />
+            </ListItem>
+            <ListItem button className={classes.nested}>
+              <ListItemText primary="Rates" />
+            </ListItem>
+          </List>
+        </Collapse>
+
+        <ListItem button onClick={handlePreferences}>
+          <ListItemText primary="Preferences" />
+          {preferences ? <LessIcon /> : <MoreIcon />}
+        </ListItem>
+        <Collapse in={preferences} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <ListItem button className={classes.nested}>
+              <ListItemText primary="General" />
+            </ListItem>
+            <ListItem button className={classes.nested}>
+              <ListItemText primary="Profile" />
+            </ListItem>
+            <ListItem button className={classes.nested}>
+              <ListItemText primary="Users" />
+            </ListItem>
+            <ListItem button className={classes.nested}>
+              <ListItemText primary="Company" />
+            </ListItem>
+            <ListItem button className={classes.nested}>
+              <ListItemText primary="Pricing" />
+            </ListItem>
+            <ListItem button className={classes.nested}>
+              <ListItemText primary="Locations" />
+            </ListItem>
+          </List>
+        </Collapse>
+
+      </List>
+    </Drawer>
+  )
+}
--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -8,12 +8,13 @@ import {
   Tabs,
   Tab,
   IconButton,
-  Avatar
+  Avatar,
+  Menu,
+  MenuItem
 } from '@material-ui/core/'
-import {
-  LocalShipping,
-  Menu
-} from '@material-ui/icons/'
+import TruckIcon from '@material-ui/icons/LocalShipping'
+import MenuIcon from '@material-ui/icons/Menu'
+import DrawerMenu from './drawer-menu'
 
 const useStyles = makeStyles(theme => ({
   toolbar: {
@@ -22,26 +23,54 @@ const useStyles = makeStyles(theme => ({
   }
 }))
 
-export default function PrimarySearchAppBar() {
+export default function NavBar() {
   const classes = useStyles()
   const [tab, setTab] = React.useState(0);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [open, setOpen] = React.useState(false);
 
   const handleChange = (event, newTab) => {
     setTab(newTab);
   };
 
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const toggleDrawer = (open) => event => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setOpen(open);
+  };
+
   return (
     <AppBar>
       <Toolbar className={classes.toolbar}>
-        <IconButton>
-          <Menu />
+        <IconButton onClick={toggleDrawer(true)}>
+          <MenuIcon />
         </IconButton>
         <Tabs value={tab} onChange={handleChange}>
-          <Tab label='DRIVERS' icon={<LocalShipping />} />
-          <Tab label='ORDERS' icon={<LocalShipping />} />
-          <Tab label='CLIENTS' icon={<LocalShipping />} />
+          <Tab label='DRIVERS' icon={<TruckIcon />} />
+          <Tab label='ORDERS' icon={<TruckIcon />} />
+          <Tab label='CLIENTS' icon={<TruckIcon />} />
         </Tabs>
-        <Avatar>IO</Avatar>
+        <Avatar onClick={handleClick}>IO</Avatar>
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem disabled>user@email.com</MenuItem>
+          <MenuItem onClick={handleClose}>Change Password</MenuItem>
+          <MenuItem onClick={handleClose}>Logout</MenuItem>
+        </Menu>
+        <DrawerMenu open={open} toggleDrawer={toggleDrawer}/>
       </Toolbar>
     </AppBar>
   )
