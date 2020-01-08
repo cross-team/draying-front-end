dc3b05af5eca0b92dda2f0a9c5ca34d472eb068c
Marcello Paiva <marcello.paiva@gmail.com>
2020-01-08 14:53:50 -0500

    fixed appbar overlap

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import NavBar from './nav-bar'
-import Container from '@material-ui/core/Container'
+import { Container } from '@material-ui/core/'
 import SEO from './seo'
 import './layout.css'
 
-const Layout = ({ children }) => (
-  <>
-    <SEO />
-    <NavBar />
-    <Container maxWidth="lg">{children}</Container>
-  </>
-)
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <SEO />
+      <NavBar />
+      <Container maxWidth="lg">{children}</Container>
+    </div>
+  )
+}
 export default Layout
