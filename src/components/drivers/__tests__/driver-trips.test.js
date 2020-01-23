d83c2f6a9ca9eecf35e41477f411b2ffcc0691d8
Marcello Paiva <marcello.paiva@gmail.com>
2020-01-23 08:29:16 -0500

    started driver-trips test

---
+++ src/components/drivers/__tests__/driver-trips.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render, fireEvent } from "@testing-library/react"
+import { MockedProvider } from "@apollo/react-testing"
+
+
+describe( 'driver-trips', () => {
+  it('renders the correct data', async () => {
+
+  }
+}
