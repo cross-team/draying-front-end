7d2ac5bb2a4b4ba1e7c1cdf5083721efb7903f7b
Marcello Paiva <marcello.paiva@gmail.com>
2020-01-24 17:06:04 -0500

    started tests

--- src/__tests__/resolvers.test.js
+++ src/__tests__/resolvers.test.js
@@ -6,19 +6,78 @@ const cache = {
   writeData: jest.fn(),
 }
 
+beforeEach(() => {
+  jest.clearAllMocks()
+})
+
+const { Mutation: { setColumnState } } = resolvers
+
 describe('resolvers.Mutation', () => {
-  it('sets columnState correctly', () => {
-    const { Mutation: { setColumnState } } = resolvers
-    cache.readQuery.mockReturnValueOnce({
+  it('maintains columnState intial state', () => {
+    cache.readQuery.mockReturnValue({
       columnState: {
         leftHidden: false, 
         middleHidden: false,
         rightHidden: false,
       }
     })
+    
     setColumnState(null, { hideLeft: false, hideMiddle: false, hideRight: true }, { cache })
-    expect(cache.readQuery).toHaveBeenCalled({
+    expect(cache.readQuery).toBeCalledWith({
       query: GET_COLUMN_STATE
+    })
+    expect(cache.writeData).toBeCalledWith({
+      data: {
+        columnState: {
+          __typename: "ColumnState",
+          leftHidden: false,
+          middleHidden: false,
+          rightHidden: true
+        }
+      }
+    })
+  })
+  it("it return intial state if passed no arguments", () => {
+    cache.readQuery.mockReturnValue({
+      columnState: {
+        leftHidden: false, 
+        middleHidden: false,
+        rightHidden: false,
+      }
+    })
+    
+    setColumnState(null, { hideLeft: undefined, hideMiddle: undefined, hideRight: undefined }, { cache })
+    expect(cache.writeData).toBeCalledWith({
+      data: {
+        columnState: {
+          __typename: "ColumnState",
+          leftHidden: false,
+          middleHidden: false,
+          rightHidden: false
+        }
+      }
+    })
+  })
+
+  it("it sets all values to true", () => {
+    cache.readQuery.mockReturnValue({
+      columnState: {
+        leftHidden: false, 
+        middleHidden: false,
+        rightHidden: false,
+      }
+    })
+
+    setColumnState(null, { hideLeft: true, hideMiddle: true, hideRight: true }, { cache })
+    expect(cache.writeData).toBeCalledWith({
+      data: {
+        columnState: {
+          __typename: "ColumnState",
+          leftHidden: true,
+          middleHidden: true,
+          rightHidden: true
+        }
+      }
     })
   })
 })
