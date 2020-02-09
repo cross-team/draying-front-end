d2a1f33597c7853a70b81f97a337685d2e657de4
Cross.Team <cross.team@CT-2TB.local>
2020-02-09 12:13:32 -0500

    added placeholder components

--- src/pages/app.js
+++ src/pages/app.js
@@ -3,12 +3,14 @@ import { Router } from '@reach/router'
 import Login from '../components/login/login'
 import PrivateRoute from '../components/private-route'
 import Drivers from '../components/drivers/drivers'
+import Orders from '../components/orders/orders'
+import Clients from '../components/clients/clients'
 
 const App = () => (
   <Router>
     <PrivateRoute path="/app/drivers" component={Drivers} />
-    <PrivateRoute path="/app/orders" component={Drivers} />
-    <PrivateRoute path="/app/clients" component={Drivers} />
+    <PrivateRoute path="/app/orders" component={Orders} />
+    <PrivateRoute path="/app/clients" component={Clients} />
     <Login path="/app/login" />
   </Router>
 )
