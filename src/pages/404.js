cd781fd245038c808264f28354a94aa12499b429
Cross.Team <cross.team@CT-2TB.local>
2020-02-09 11:49:58 -0500

    change to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,6 @@
 import React from 'react'
+import Index from '.'
 
-import Layout from '../components/layout'
-
-const NotFoundPage = () => (
-  <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const NotFoundPage = () => <Index />
 
 export default NotFoundPage
