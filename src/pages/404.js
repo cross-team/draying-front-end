9ae21996e1ed53856dee51b5bb6cbaa034b085da
Cross.Team <cross.team@CT-2TB.local>
2020-02-09 12:25:36 -0500

    link to home on 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 const NotFoundPage = () => (
   <>
     <h1>NOT FOUND</h1>
     <p>Redirecting</p>
+
+    <Link to="/">Go To Home -></Link>
   </>
 )
 
