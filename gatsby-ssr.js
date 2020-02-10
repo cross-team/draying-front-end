f2b9a46dc3c17a31e4a94627b39465d08610a9f2
Marcello Paiva <marcello.paiva@gmail.com>
2020-02-10 14:25:06 -0500

    loading scripts with helmet

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,11 +1,4 @@
-const React = require('react')
 require(`isomorphic-fetch`)
 const wrapRootElement = require('./src/wrap-root-element').default
 
 exports.wrapRootElement = wrapRootElement
-
-exports.onRenderBody = ({ setHeadComponents }) => {
-  setHeadComponents([
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCwLTFeBJLrgR7zOb3zbBwRFVWOLZJO0Tw&libraries=places"></script>,
-  ])
-}
--- src/components/location-input .js
+++ src/components/location-input .js
@@ -7,6 +7,7 @@ import PlacesAutocomplete, {
   geocodeByAddress,
   getLatLng,
 } from 'react-places-autocomplete'
+import { Helmet } from 'react-helmet'
 
 const useStyles = makeStyles(theme => ({
   input: {
@@ -37,59 +38,64 @@ const LocationInput = ({ updateLocation, appendCoordinates }) => {
   }
 
   return (
-    <PlacesAutocomplete
-      value={address}
-      onChange={handleChange}
-      onSelect={handleSelect}
-    >
-      {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-        <Grid container>
-          <TextField
-            className={classes.input}
-            variant="outlined"
-            label="Location Address"
-            value={address}
-            inputProps={{
-              ...getInputProps({
-                className: 'location-search-input',
-              }),
-            }}
-          />
-          <Grid container direction="column">
-            {loading && <Typography>Loading...</Typography>}
-            {suggestions.map(suggestion => {
-              const className = suggestion.active
-                ? 'suggestion-item--active'
-                : 'suggestion-item'
-              // inline style for demonstration purpose
-              const style = suggestion.active
-                ? {
-                    backgroundColor: '#fafafa',
-                    cursor: 'pointer',
-                    minHeight: '40px',
-                  }
-                : {
-                    backgroundColor: '#ffffff',
-                    cursor: 'pointer',
-                    minHeight: '40px',
-                  }
-              return (
-                <Grid
-                  container
-                  alignItems="center"
-                  {...getSuggestionItemProps(suggestion, {
-                    className,
-                    style,
-                  })}
-                >
-                  <Typography>{suggestion.description}</Typography>
-                </Grid>
-              )
-            })}
+    <>
+      <Helmet>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCwLTFeBJLrgR7zOb3zbBwRFVWOLZJO0Tw&libraries=places"></script>
+      </Helmet>
+      <PlacesAutocomplete
+        value={address}
+        onChange={handleChange}
+        onSelect={handleSelect}
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <Grid container>
+            <TextField
+              className={classes.input}
+              variant="outlined"
+              label="Location Address"
+              value={address}
+              inputProps={{
+                ...getInputProps({
+                  className: 'location-search-input',
+                }),
+              }}
+            />
+            <Grid container direction="column">
+              {loading && <Typography>Loading...</Typography>}
+              {suggestions.map(suggestion => {
+                const className = suggestion.active
+                  ? 'suggestion-item--active'
+                  : 'suggestion-item'
+                // inline style for demonstration purpose
+                const style = suggestion.active
+                  ? {
+                      backgroundColor: '#fafafa',
+                      cursor: 'pointer',
+                      minHeight: '40px',
+                    }
+                  : {
+                      backgroundColor: '#ffffff',
+                      cursor: 'pointer',
+                      minHeight: '40px',
+                    }
+                return (
+                  <Grid
+                    container
+                    alignItems="center"
+                    {...getSuggestionItemProps(suggestion, {
+                      className,
+                      style,
+                    })}
+                  >
+                    <Typography>{suggestion.description}</Typography>
+                  </Grid>
+                )
+              })}
+            </Grid>
           </Grid>
-        </Grid>
-      )}
-    </PlacesAutocomplete>
+        )}
+      </PlacesAutocomplete>
+    </>
   )
 }
 
